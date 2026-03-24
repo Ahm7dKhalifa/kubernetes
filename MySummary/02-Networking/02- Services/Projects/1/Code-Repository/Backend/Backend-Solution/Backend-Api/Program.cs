ddac89ac26a03905ee1c158a78c87bc583f71634
ahmed khalifa <ahm7dkhalifa@gmail.com>
2026-03-24 13:30:45 +0200

    add index.html front end , cors to backend

--- MySummary/02-Networking/02- Services/Projects/1/Code-Repository/Backend/Backend-Solution/Backend-Api/Program.cs
+++ MySummary/02-Networking/02- Services/Projects/1/Code-Repository/Backend/Backend-Solution/Backend-Api/Program.cs
@@ -10,6 +10,15 @@ namespace Backend_Api
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -24,6 +33,7 @@ namespace Backend_Api
             }
 
             app.UseHttpsRedirection();
+            app.UseCors("AllowAll");
 
             app.UseAuthorization();
 
