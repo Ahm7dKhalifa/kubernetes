dd4af3b367a79385bf116c37fca8baaa44ca27d0
ahmed khalifa <ahm7dkhalifa@gmail.com>
2026-06-09 00:07:12 +0300

    add k8s-scheduling-v2 file

--- MySummary/02-Networking/02- Services/Projects/1/Code-Repository/Backend/Backend-Solution/Backend-Api/obj/Release/net8.0/Backend-Api.AssemblyInfo.cs
+++ MySummary/02-Networking/02- Services/Projects/1/Code-Repository/Backend/Backend-Solution/Backend-Api/obj/Release/net8.0/Backend-Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend-Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b48ee161ea3d3a24ab087be17abe8af2a4e2b5a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+890c1d7274ff05f114f5bf17d251fa2e7cf6a0e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend-Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend-Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
